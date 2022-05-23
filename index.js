9b802eb1d16c2c7669786a2f5e9befc38d76542d
Felipe Nonato Carvalho Matos <felipe.matos@awscode.com.br>
2022-05-23 11:33:09 -0300

    feat: initial project structure with individual instructor listing and consultation

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+
+const app = express();
+
+// GET /instrutores <= OK
+
+const listaDeInstrutores = [
+  {
+    id: 1,
+    nome: "Junior",
+    idade: 22,
+    areaDeAtuacao: "Back-end",
+  },
+  {
+    id: 2,
+    nome: "Fernando",
+    idade: 28,
+    areaDeAtuacao: "Back-end",
+  },
+  {
+    id: 3,
+    nome: "Felipe",
+    idade: 28,
+    areaDeAtuacao: "Front-end",
+  },
+  {
+    id: 4,
+    nome: "Júlio",
+    idade: 40,
+    areaDeAtuacao: "FullStack",
+  },
+  {
+    id: 5,
+    nome: "Luis",
+    idade: 30,
+    areaDeAtuacao: "Back-end",
+  },
+  {
+    id: 6,
+    nome: "Alex",
+    idade: 20,
+    areaDeAtuacao: "Front-end",
+  },
+];
+
+app.get("/instrutores", (req, res) => {
+  res.json(listaDeInstrutores);
+});
+
+app.get("/instrutores/:idConsultado", (req, res) => {
+  const instrutor = listaDeInstrutores.find(
+    (instrutor) => instrutor.id === Number(req.params.idConsultado)
+  );
+  res.json(instrutor);
+});
+
+app.listen(8000);
